175a9f57c251e7295531af132fb0601fcb239a9e
Fillan Lee <fillanlee@aol.com>
2019-02-22 11:32:56 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,23 +2,14 @@ class Hash
   def keys_of(arguments, *other)
     return_val = []
     
-    # other.do each |val|
+  
       self.each do |k, v|
          if arguments == v 
           return_val << k
-        else
-          other.each do |item|
-            if other == v
-              if return_val.include?(k)
-                
-              else
-                return_val << k
-              end
-          end
          end
        end
       end
-    # end
+    
        return_val 
     
    end
